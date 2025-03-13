5f1e0ad86c05331c87678b0b6bb27b84224a4408
natheesh <natheeshkumar4656@gmail.com>
2025-03-13 18:07:37 +0530

    js basics

--- Learning1/ReactPractice/mystudypractice/src/Component/array methods ES/LearnY/ArrayMethodsFY.js
+++ Learning1/ReactPractice/mystudypractice/src/Component/array methods ES/LearnY/ArrayMethodsFY.js
@@ -9,16 +9,91 @@ function ArrayMethodsFY() {
 let num=[10,20,30,40];
 let num1= num.map(val=>val*2);
 
+//
+const fruits=["apple","banana","mango"];
+
+
+const fruitList = fruits.join(', ');
 // console.log(num1);
 
+const fruitListarr = fruitList.split(",");
 
+const name = "Alice";
+const age = 30;
+// console.log("My name is ".concat(name).concat(" and I am ").concat(age).concat(" years old."));
+
+// const arr = ['Hello', 'World', '!'];
+// const str = arr.toString();
+// console.log(str); // "Hello,World,!;"
+
+// const str1 = "Hello";
+// const arr1 = Array.from(str1);
+// console.log(arr1); // ["H", "e", "l", "l", "o"]
+
+
+const arr6 = ['Hello',  'World',       '!'];
+const str6 = arr6.join(','); // Join with a space
+console.log(str6); // "Hello World !"
+
+
+const str7 = "Hello,   World,   !";
+const arr7 = str7.split(",   "); // Splits based on the comma and space
+console.log(arr7); // ["Hello", "World!"]
+
+
+const arr8 = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 30 }
+];
+const str8 = JSON.stringify(arr8);
+console.log(str8); // '[{"name":"Alice","age":25},{"name":"Bob","age":30}]'
 
     return (
         <div>
-            <h1>{num1}</h1>
+            {/* <h1>{num1}</h1>
             
-            { console.log(num1)
-}
+            { console.log(num1)} */}
+{/* 
+            <ul>
+                {
+                    fruits.map((fruit,index)=>(<li >{` i ma a ${fruit}`}</li>))
+                }
+
+                {
+                    
+                <div>{`Fruits available are: ${fruitList}`} <br></br></div>
+                }
+            </ul> */}
+
+
+                    {/* <p>
+                    {fruits.map((fruit, index) => (
+                        <span key={index}>
+                        {` ${fruit}`}<br />
+                        </span>
+                    ))}
+                    </p> */}
+
+                    
+                    <p>
+                    {fruitListarr.map((fruitList1, index) => (
+                        <span key={index}>
+                        {` block element ${fruitList1}`}<br />
+                        </span>
+                    ))}
+                    </p>
+
+
+
+                    
+            <ul>
+                {
+                    fruitListarr.map((fruit,index)=>(
+                    <li >{` using list ${fruit}`}</li>
+                ))}
+            </ul>
+
+                    {/* {console.log("My name is ".concat(name).concat(" and I am ").concat(age).concat(" years old."))} */}
         </div>
     )
 }
